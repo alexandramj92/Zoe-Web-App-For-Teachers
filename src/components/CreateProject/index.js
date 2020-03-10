587f3167acff9ccfb792e602694e7e62dc17e778
Peter Rydell <peterydell@gmail.com>
2020-03-09 20:11:46 -0700

    Creat New Project styling updates

--- src/components/CreateProject/index.js
+++ src/components/CreateProject/index.js
@@ -4,6 +4,7 @@ import { Container } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 
 require('./style.css');
 
@@ -11,7 +12,7 @@ const useStyles = makeStyles(theme => ({
     root: {
       '& .MuiTextField-root': {
         margin: theme.spacing(1),
-        width: 200,
+        flexGrow: 1,
       },
     },
   }));
@@ -26,36 +27,53 @@ export default function MultilineTextFields() {
   return (
       <Container id="heading" maxWidth="sm">
       <h1>Create a New Project</h1>
-      <Box display="flex" flexDirection="row" justifyContent="flex-start">
-        <div class="input">
-            <h2>Project Name</h2>
-            <TextField
-            id="outlined-static"
-            multiline
-            rows="1"
-            defaultValue="Name"
-            variant="outlined"
-            class="textField"
-            />
-        </div>
-      </Box>
-      <Box display="flex" flexDirection="row" justifyContent="flex-start">
-        <div class="input">
-            <h2>Project Description</h2>
-            <TextField
-            id="outlined-multiline-static"
-            multiline
-            rows="4"
-            defaultValue="Description of the Project"
-            variant="outlined"
-            class="textField"
-            />
-        </div>
-      </Box>
-    <Button variant="contained" id="cancel">Cancel</Button>
-    <Button variant="contained" id="next">Next</Button>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Box display="flex" justifyContent="flex-start">
+            <div id="nameBox">
+                <h2>Project Name</h2>
+                <Grid item xs={12}>
+                  <TextField
+                  defaultValue="Name"
+                  variant="filled"
+                  class="textField"
+                  justify="flex-start"
+                  fullWidth
+                  />
+                </Grid>
+            </div>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <Box display="flex" justifyContent="flex-start">
+            <div id="descriptionBox">
+                <h2>Project Description</h2>
+                <Grid item xs={12}>
+                  <TextField
+                  multiline
+                  rows="6"
+                  defaultValue="Description of the Project"
+                  variant="filled"
+                  class="textField"
+                  justify="flex-start"
+                  fullWidth
+                  />
+                </Grid>
+            </div>
+          </Box>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12} sm={4}>
+          <Button variant="contained" id="cancel">Cancel</Button>
+        </Grid>
+        <Grid item xs={12} sm={4}></Grid>
+        <Grid item xs={12} sm={4}>
+          <Button variant="contained" id="next">Next</Button>
+        </Grid>
+      </Grid>
 
-      </Container>
+  </Container>
     
     
   );
